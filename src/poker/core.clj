b820c7fd1253ac9042100d3c22b3085a63a97e92
Doug Knight <dknight@getaroom.com>
2020-06-03 19:59:17 -0500

    finish deck implementation

--- src/poker/core.clj
+++ src/poker/core.clj
@@ -2,16 +2,43 @@
   (:gen-class))
 
 (def suits #{:hearts :diamonds :spades :clubs})
+(def ranks #{:A :2 :3 :4 :5 :6 :7 :8 :9 :10 :J :Q :K})
+
+(defn card?
+  [card]
+  (vector? card))
+
+(defn- suit?
+  [card suit]
+  {:pre [(card? card)]}
+
+  (= suit (second card)))
+
+(defn rank
+  [card]
+  {:pre [(card? card)]}
+
+  (first card))
 
 (defn diamond?
   [card]
-  {:pre [(vector? card)]}
+  (suit? card :diamonds))
 
-  (= :diamonds (second card)))
+(defn heart?
+  [card]
+  (suit? card :hearts))
+
+(defn spade?
+  [card]
+  (suit? card :spades))
+
+(defn club?
+  [card]
+  (suit? card :clubs))
 
 (defn deck []
   (mapcat (fn [suit]
             (map (fn [number]
                    [number suit])
-                 (range 13)))
+                 ranks))
           suits))
--- test/poker/core_test.clj
+++ test/poker/core_test.clj
@@ -12,6 +12,9 @@
   (let [cards (poker/deck)]
     (is (= 52 (count cards))
         "A deck has 52 cards")
-    (is (= 13 (count (filter (fn [card]
-                               (poker/diamond? card))
-                             cards))))))
+    (doseq [f [poker/diamond? poker/heart? poker/spade? poker/club?]]
+      (is (= 13 (count (filter #(f %) cards)))
+          (str "The deck contains 13 cards of suit " f)))
+    (doseq [rank [:A :2 :3 :4 :5 :6 :7 :8 :9 :10 :J :Q :K]]
+      (is (= 4 (count (filter #(= rank (poker/rank %)) cards)))
+          (str "The deck contains 4 cards of rank " rank)))))
